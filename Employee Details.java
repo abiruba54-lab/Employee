00ce081d8efa32ea7a4fdc157479c3a82b93cc72
abiruba54-lab <abiruba54@gmail.com>
2025-09-28 20:34:49 +0530

    Add files via upload

---
+++ Employee Details.java
@@ -0,0 +1,69 @@
+package src;
+
+public class project1 {
+    int emplo[] = {1001, 1002, 1003, 1004, 1005, 1006, 1007};
+    String empname[] = {"Abi", "Subi", "Gobi", "Aarthi", "Gowri", "Harini", "Jay"};
+    String joindate[] = {"01/04/2009", "23/08/2012", "12/11/2008", "29/01/2013", "16/07/2005", "1/1/2000", "12/06/2006"};
+    char designationCode[] = {'e', 'c', 'k', 'r', 'm', 'e', 'c'};
+    String dept[] = {"R&D", "PM", "Acct", "Front Desk", "Engg", "Manufacturing", "PM"};
+    int basic[] = {20000, 30000, 10000, 12000, 50000, 23000, 29000};
+    int hra[] = {8000, 12000, 8000, 6000, 20000, 9000, 12000};
+    int it[] = {3000, 9000, 1000, 2000, 20000, 4400, 10000};
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide employee id as argument.");
+            return;
+        }
+
+        int empId = Integer.parseInt(args[0]);
+        project1 obj = new project1();
+        obj.showDetails(empId);
+    }
+
+    void showDetails(int empId) {
+        boolean found = false;
+
+        for (int i = 0; i < emplo.length; i++) {
+            if (emplo[i] == empId) {
+                found = true;
+
+                String designation = "";
+                int da = 0;
+
+                switch (designationCode[i]) {
+                    case 'e':
+                        designation = "Engineer";
+                        da = 20000;
+                        break;
+                    case 'c':
+                        designation = "Consultant";
+                        da = 32000;
+                        break;
+                    case 'k':
+                        designation = "Clerk";
+                        da = 12000;
+                        break;
+                    case 'r':
+                        designation = "Receptionist";
+                        da = 15000;
+                        break;
+                    case 'm':
+                        designation = "Manager";
+                        da = 40000;
+                        break;
+                }
+
+                int salary = basic[i] + hra[i] + da - it[i];
+
+                System.out.println("Emp No.\tEmp Name\tDepartment\tDesignation\tSalary");
+                System.out.println(emplo[i] + "\t" + empname[i] + "\t\t" + dept[i] + "\t" + designation + "\t" + salary);
+                break; 
+            }
+        }
+
+        if (!found) {
+            System.out.println("There is no employee with empid : " + empId);
+        }
+    }
+}
